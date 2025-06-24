136a7d7162362de9fa96041eb07d27612f971ac5
v605690 <v605690@gmail.com>
2025-06-24 14:54:59 -0500

    fundamentals, Excercise_03, Excercise_04, Excercise_05

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,22 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        int number = 12;
+        System.out.println("number is " + number);
+        long longNumber = 123456434342L;
+        System.out.println("long number is " + longNumber);
+        byte byteNumber = 127;
+        System.out.println("byte number is " + byteNumber);
+        short shortNumber = 23;
+        System.out.println("short number is " + shortNumber);
+        boolean result = true;
+        System.out.println("boolean result is " + result);
+        char character = 'p';
+        System.out.println("char character is " + character);
+        float floatNumber = 1.33F;
+        System.out.println("float number is " + floatNumber);
+        double doubleNumber = 29.99;
+        System.out.println("double number is " + doubleNumber);
 
     }
-
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import java.util.Scanner;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -14,14 +16,30 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int i = str.length();
+        System.out.println("str length is " + i);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean b1 = str.equals(str2);
+        System.out.println("b1 is " + b1);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String newString = str + " " + str2;
+        System.out.println("New string is " + newString);
+        System.out.println(str.concat(str2));
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println(str.contains("hel"));
+        System.out.println(str.length());
+        System.out.println(str2.replace("hello", "Greetings"));
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter a number: ");
+        int num2 = scan.nextInt();
+        int sum = num1 + num2;
+        System.out.println("sum is " + sum);
 
     }
 

d2b5a0dc9e2cea98ecc12e69e2ac155df8e553af
v605690 <v605690@gmail.com>
2025-07-03 10:45:31 -0500

    methods/Exercise_01

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -16,13 +16,24 @@ public class Exercise_01 {
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
 
+    public static int multiply(int a, int b) {
+
+        return (a * b);
+    }
+
+
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
 
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
-
+    public static void myJoke() {
+        System.out.println("Uh...lemme think...... Lemme think.. Java doesn't have garbage collection, because if it did, it would delete itself.");
+    }
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
     //    and returns the number of seconds that number in years represents
